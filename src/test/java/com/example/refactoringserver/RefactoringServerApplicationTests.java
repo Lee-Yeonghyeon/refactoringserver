e554e0478f1ad7e0bee774c8c0690131c9f5d755
wadeyoon-blue <56951080+wadeyoon-blue@users.noreply.github.com>
2022-07-05 17:28:25 +0900

    Update RefactoringServerApplicationTests.java

--- src/test/java/com/example/refactoringserver/RefactoringServerApplicationTests.java
+++ src/test/java/com/example/refactoringserver/RefactoringServerApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.refactoringserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// test
 @SpringBootTest
 class RefactoringServerApplicationTests {
 
