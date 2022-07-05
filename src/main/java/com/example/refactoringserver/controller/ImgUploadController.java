0624ba8e2732cbb8cb4a506746b3b8e35f1d2dc5
wadeyoon-blue <56951080+wadeyoon-blue@users.noreply.github.com>
2022-07-05 17:31:40 +0900

    Update ImgUploadController.java

--- src/main/java/com/example/refactoringserver/controller/ImgUploadController.java
+++ src/main/java/com/example/refactoringserver/controller/ImgUploadController.java
@@ -13,6 +13,7 @@ public class ImgUploadController {
 
     private final S3Uploader s3Uploader;
 
+    // todo 결과를 json 으로 리턴할 수 있도록 변경해주세요.
     @PostMapping("/images")
     public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
         s3Uploader.upload(multipartFile, "static");
