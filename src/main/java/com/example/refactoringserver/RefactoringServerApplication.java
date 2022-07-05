36ba2926b7b922844d1be110d86ca1f57b5a0cd9
이영현 <iyeonghyeon@iyeonghyeon-ui-MacBookPro.local>
2022-07-05 10:19:54 +0900

    Initial commit

---
+++ src/main/java/com/example/refactoringserver/RefactoringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.refactoringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/refactoringserver/RefactoringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refactoringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
